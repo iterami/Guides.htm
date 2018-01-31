d8beda993d4e680a02647992068e78b369e16a30
Jan Hořava <czechinthemail@gmail.com>
2018-01-31 12:13:42 -0800

    improved formatting

--- eve/js/tools.js
+++ eve/js/tools.js
@@ -4,32 +4,28 @@ function calculate_alignment(){
     var inertia = Number(document.getElementById('alignment-inertia').value);
     var mass = Number(document.getElementById('alignment-mass').value);
 
-    document.getElementById('alignment-result').innerHTML =
-      (Math.log(2) * inertia * mass) / 500000;
+    document.getElementById('alignment-result').innerHTML = (Math.log(2) * inertia * mass) / 500000;
 }
 
 function calculate_efficiency(){
     var lost = Number(document.getElementById('efficiency-lost').value);
     var total = lost + Number(document.getElementById('efficiency-destroyed').value);
 
-    document.getElementById('efficiency-result').innerHTML =
-      (1 - (lost  / total)) * 100;
+    document.getElementById('efficiency-result').innerHTML = (1 - (lost  / total)) * 100;
 }
 
 function calculate_skillpoints(){
     var level = Number(document.getElementById('skill-level').value);
     var rank = Number(document.getElementById('skill-rank').value);
 
-    document.getElementById('skill-result').innerHTML =
-      Math.floor(Math.pow(2, 2.5 * (level - 1)) * rank * 250);
+    document.getElementById('skill-result').innerHTML = Math.floor(Math.pow(2, 2.5 * (level - 1)) * rank * 250);
 }
 
 function calculate_target(){
     var scan = Number(document.getElementById('target-scan').value);
     var sig = Number(document.getElementById('target-sig').value);
 
-    document.getElementById('target-result').innerHTML =
-      40000 / (scan * Math.pow(Math.sin(sig), 2));
+    document.getElementById('target-result').innerHTML = 40000 / (scan * Math.pow(Math.sin(sig), 2));
 }
 
 function calculate_year(){
