621a4026f08d0655beea97ac0a3b958311446c8b
Jan Hořava <czechinthemail@gmail.com>
2020-05-23 15:46:57 -0700

    started cookies/time calculator

--- cookie-clicker/js/tools.js
+++ cookie-clicker/js/tools.js
@@ -8,6 +8,29 @@ function calculate_magiccost(){
     document.getElementById('cost-total').textContent = Math.floor(base + max * (percent / 100));
 }
 
+function calculate_time(){
+    let cps = Number(document.getElementById('time').value) || 1;
+
+    if(cps <= 0){
+        cps = 1;
+    }
+
+    const times = {
+      'cookies-per-day': 86400,
+      'cookies-per-hour': 3600,
+      'cookies-per-minute': 60,
+      'cookies-per-week': 604800,
+    };
+    for(const time in times){
+        document.getElementById(time).value = core_number_format({
+          'decimals-max': 3,
+          'number': core_round({
+            'number': cps * times[time],
+          }),
+        });
+    }
+}
+
 function repo_init(){
     core_repo_init({
       'root': '../../index.htm',
@@ -17,4 +40,5 @@ function repo_init(){
     document.getElementById('max-magic').oninput =
       document.getElementById('cost-base').oninput =
       document.getElementById('cost-percent').oninput = calculate_magiccost;
+    document.getElementById('time').oninput = calculate_time;
 };
