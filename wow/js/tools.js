ee2ee61463a2695b8d1e334824b5961bd99d7a1d
Jan Hořava <czechinthemail@gmail.com>
2020-06-16 19:46:56 -0700

    started tools, more items and rare mobs

---
+++ wow/js/tools.js
@@ -0,0 +1,27 @@
+'use strict';
+
+function calculate_health(){
+    const health = Number(document.getElementById('health-max').value) || 1;
+
+    document.getElementById('health-bloodthirst').textContent = core_round({
+      'number': health * (5 / 100),
+    });
+    document.getElementById('health-bloodthirst-fresh-meat').textContent = core_round({
+      'number': health * (6 / 100),
+    });
+    document.getElementById('health-healthstone').textContent = core_round({
+      'number': health * (25 / 100),
+    });
+    document.getElementById('health-victory-rush').textContent = core_round({
+      'number': health * (20 / 100),
+    });
+}
+
+function repo_init(){
+    core_repo_init({
+      'root': '../../index.htm',
+      'title': 'Guides.htm',
+    });
+
+    document.getElementById('health-max').oninput = calculate_health;
+};
